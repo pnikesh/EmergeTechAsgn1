f8b2dddfb6ef5b598758c7e21b06d2e7dfeb3197
Nik <nikeshpatel356@gmail.com>
2019-02-16 22:03:45 -0500

    published on mLab and Huroku

--- config/db.js
+++ config/db.js
@@ -7,8 +7,9 @@ Feb 16, 2019 */
 
 module.exports = {
   //local mongo db deployment
-  URI: "mongodb://localhost/asgn1"
+  //URI: "mongodb://localhost/asgn1"
   //"URI":"mongodb://localhost/nikesh_patel"
   //"URI":"mongodb://nik:password123@ds058739.mlab.com:58739/exc_4"
   //"URI":"mongodb://nik:password123@ds060977.mlab.com:60977/exc_4"
+  URI:"mongodb://nik:nik123@ds060977.mlab.com:60977/asgn1"
 };
